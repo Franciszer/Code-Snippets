4363cda935b9d415f4be5c235a0f9496cf872172
Franciszer <ftthierree@gmail.com>
2020-10-16 11:44:17 +0200

    updated script

--- set_snippets.sh
+++ set_snippets.sh
@@ -5,3 +5,8 @@ then
 	cp * /Users/frthierr/Library/Application\ Support/Code/User/snippets
 	echo "OK"
 fi
+if [[ $1 = 'linux' ]]
+then
+	cp * "/home/franciszer/.config/Code/User/snippets"
+	echo "OK"
+fi
